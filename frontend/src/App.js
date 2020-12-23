7c3b626dfe474b845ab2f18a72a43f4c2f6f5230
ehsangh7 <ehsanghochagh@gmail.com>
2020-12-24 00:38:42 +0330

    modified App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Checkout from './components/Checkout';
+import Login from './components/auth/Login';
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { useStateValue } from './components/StateProvider';
 
 function App() {
+  const [{}, dispatch] = useStateValue()
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log(authUser);
+
+      if(authUser) {
+        // the user just logged in / the user was logged in
+        console.log("USer>>", authUser);
+        dispatch({
+          type:'SET_USER',
+          user: authUser,
+        })
+      }else{
+        // the user logged out 
+        dispatch({
+          type:'SET_USER',
+          user: null,
+        })
+      }
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    	<div className="app">
+        <Header />
+    	  <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/login" component={Login} />
+        </Switch>
+    	</div>
+    </Router>
   );
 }
 
