f31e1206c5549129c50b099ac511e81acee1b0a7
ehsangh7 <ehsanghochagh@gmail.com>
2020-12-24 15:53:11 +0330

    add admin.py in product

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+@admin.register(models.Product)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('id' ,'title' , 'price' , 'rating')
+    prepopulated_fields = {'title': ('title',), }
+
+
+admin.site.register(models.Category)
