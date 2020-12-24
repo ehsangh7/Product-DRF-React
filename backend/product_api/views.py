509be8fe4865a5022871d622f6a246347d4c0bf1
ehsangh7 <ehsanghochagh@gmail.com>
2020-12-24 15:54:47 +0330

    create views.py i API

--- backend/product_api/views.py
+++ backend/product_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from product.models import Product, Order, OrderItem
+from .serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
+from rest_framework import generics
 
 # Create your views here.
+
+
+
+class ProductList(generics.ListAPIView):
+
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
